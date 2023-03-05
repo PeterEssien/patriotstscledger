51c0f8626980e4102a4ac3fcab8c9676f7a85f35
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2023-03-05 01:41:36 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,7 @@ import FeaturesBlocks from '../partials/FeaturesBlocks';
 import Testimonials from '../partials/Testimonials';
 import Newsletter from '../partials/Newsletter';
 import Footer from '../partials/Footer';
-import Footer from '../partials/Banner';
-
+import Banner from '../partials/Banner';
 
 
 function Home() {
