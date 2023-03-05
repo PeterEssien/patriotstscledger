46cab768f51fbddc602b28ffba8d1254f193972e
Peter Essien <essienpeter57@gmail.com>
2023-03-05 15:30:14 +0000

    Update SignUp.jsx and Banner.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,145 +1,286 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Header from '../partials/Header';
-import Banner from '../partials/Banner';
+import Header from "../partials/Header";
+import Banner from "../partials/Banner";
 
 function SignUp() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-
       {/*  Site header */}
       <Header />
 
       {/*  Page content */}
       <main className="flex-grow">
-
         <section className="bg-gradient-to-b from-gray-100 to-white">
           <div className="max-w-6xl mx-auto px-4 sm:px-6">
             <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
               {/* Page header */}
               <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                <h3 className="h1">Customer And Shipping Info.</h3>
+                <h3 className="h1">Customer And Shipping Info</h3>
               </div>
 
               {/* Form */}
               <div className="max-w-sm mx-auto">
                 <form>
-
-                 <div className="flex flex-wrap -mx-3 mb-4">
-                    <div className="w-full px-3">
-                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
-                      <input id="email" type="email" className="form-input w-full text-gray-800" required />
-                    </div>
-                  </div>
-
-                   <div className="flex flex-wrap -mx-3 mb-4">
-                    <div className="w-full px-3">
-                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">First Name <span className="text-red-600">*</span></label>
-                      <input id="name" type="text" className="form-input w-full text-gray-800" required />
-                    </div>
-                  </div>
-
-                   <div className="flex flex-wrap -mx-3 mb-4">
-                    <div className="w-full px-3">
-                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Last Name <span className="text-red-600">*</span></label>
-                      <input id="name" type="text" className="form-input w-full text-gray-800" required />
-                    </div>
-                  </div>
-
-                   <div className="flex flex-wrap -mx-3 mb-4">
-                    <div className="w-full px-3">
-                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Address Line 1 <span className="text-red-600">*</span></label>
-                      <input id="name" type="text" className="form-input w-full text-gray-800" required />
-                    </div>
-                  </div>
-
-                   <div className="flex flex-wrap -mx-3 mb-4">
-                    <div className="w-full px-3">
-                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Country <span className="text-red-600">*</span></label>
-                      <input id="name" type="text" className="form-input w-full text-gray-800" text="United State of America" required />
-                    </div>
-                  </div>
-
-                   <div className="flex flex-wrap -mx-3 mb-4">
-                    <div className="w-full px-3">
-                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">City <span className="text-red-600">*</span></label>
-                      <input id="name" type="text" className="form-input w-full text-gray-800" required />
-                    </div>
-                  </div>
-
-                   <div className="flex flex-wrap -mx-3 mb-4">
-                    <div className="w-full px-3">
-                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">State <span className="text-red-600">*</span></label>
-                      <input id="name" type="text" className="form-input w-full text-gray-800" required />
-                    </div>
-                  </div>
-
-                   <div className="flex flex-wrap -mx-3 mb-4">
-                    <div className="w-full px-3">
-                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Zip Code <span className="text-red-600">*</span></label>
-                      <input id="name" type="number" className="form-input w-full text-gray-800" required />
-                    </div>
-                  </div>
-
-                   <div className="flex flex-wrap -mx-3 mb-4">
-                    <div className="w-full px-3">
-                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="name">Telephone <span className="text-red-600">*</span></label>
-                      <input id="name" type="tel" className="form-input w-full text-gray-800" required />
-                    </div>
-                  </div>
-
-                <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                <h4 className="h1">Shipping Method</h4>
-              </div>
-
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
-                       <input type="radio" id="fedex" name="shipping_method" value="FEDEX"/>
-                       <label for="fedex">Express Shipping with Fedex Express (2-3 Business Days)</label>
-                       <h4>FREE</h4>
-                   </div>
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="email"
+                      >
+                        Email <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="email"
+                        type="email"
+                        className="form-input w-full text-gray-800"
+                        required
+                      />
+                    </div>
                   </div>
 
                   <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
-                       <input type="radio" id="dhl" name="shipping_method" value="DHL"/>
-                       <label for="fedex">Express Shipping with DHL Express (2-4 Business Days)</label>
-                       <h4>FREE</h4>
-                   </div>
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="name"
+                      >
+                        First Name <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="name"
+                        type="text"
+                        className="form-input w-full text-gray-800"
+                        required
+                      />
+                    </div>
                   </div>
 
-                 
-                <div className="flex items-center my-6">
-                  <div className="border-t border-gray-300 flex-grow mr-3" aria-hidden="true"></div>
-                  <div className="text-gray-600 italic">Before proceeding to the next page please make sure you have purchased the amount of Bitcoin you want to store on the TSC Ledger</div>
-                  <div className="border-t border-gray-300 flex-grow ml-3" aria-hidden="true"></div>
-                </div>
-
-               <div className="flex flex-wrap -mx-3 mt-6">
+                  <div className="flex flex-wrap -mx-3 mb-4">
                     <div className="w-full px-3">
-                      <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Continue</button>
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="name"
+                      >
+                        Last Name <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="name"
+                        type="text"
+                        className="form-input w-full text-gray-800"
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="name"
+                      >
+                        Address Line 1 <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="name"
+                        type="text"
+                        className="form-input w-full text-gray-800"
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="name"
+                      >
+                        Address Line 2(Optional){" "}
+                      </label>
+                      <input
+                        id="name"
+                        type="text"
+                        className="form-input w-full text-gray-800"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="name"
+                      >
+                        Country <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="name"
+                        type="text"
+                        className="form-input w-full text-gray-800"
+                        value="United State of America"
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="name"
+                      >
+                        City <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="name"
+                        type="text"
+                        className="form-input w-full text-gray-800"
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="name"
+                      >
+                        State <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="name"
+                        type="text"
+                        className="form-input w-full text-gray-800"
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="name"
+                      >
+                        Zip Code <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="name"
+                        type="number"
+                        className="form-input w-full text-gray-800"
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label
+                        className="block text-gray-800 text-sm font-medium mb-1"
+                        htmlFor="name"
+                      >
+                        Telephone <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        id="name"
+                        type="tel"
+                        className="form-input w-full text-gray-800"
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                    <h4 className="h1">Shipping Method</h4>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <input
+                        type="radio"
+                        id="fedex"
+                        name="shipping_method"
+                        value="FEDEX"
+                      />
+                      <label for="fedex">
+                        Express Shipping with Fedex Express (2-3 Business Days)
+                      </label>
+                      <b>
+                        <h4>FREE</h4>
+                      </b>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <input
+                        type="radio"
+                        id="dhl"
+                        name="shipping_method"
+                        value="DHL"
+                      />
+                      <label for="fedex">
+                        Express Shipping with DHL Express (2-4 Business Days)
+                      </label>
+                      <b>
+                        <h4>FREE</h4>
+                      </b>
+                    </div>
+                  </div>
+
+                  <div className="flex items-center my-6">
+                    <div
+                      className="border-t border-gray-300 flex-grow mr-3"
+                      aria-hidden="true"
+                    ></div>
+                    <div className="text-gray-600 italic">
+                      Before proceeding to the next page please make sure you
+                      have purchased the amount of Bitcoin you want to store on
+                      the TSC Ledger
+                    </div>
+                    <div
+                      className="border-t border-gray-300 flex-grow ml-3"
+                      aria-hidden="true"
+                    ></div>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mt-6">
+                    <div className="w-full px-3">
+                      <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">
+                        Continue
+                      </button>
                     </div>
                   </div>
                   <div className="text-sm text-gray-500 text-center mt-3">
-                    Patriots Ledger collects and processes your personal data to manage your order. <a className="underline" href="#0"><b>Learn more about managing your data and your rights.</b></a>
-                                </div>
+                    Patriots Ledger collects and processes your personal data to
+                    manage your order.{" "}
+                    <a className="underline" href="#0">
+                      <b>
+                        Learn more about managing your data and your rights.
+                      </b>
+                    </a>
+                  </div>
                 </form>
 
                 <div className="text-gray-600 text-center mt-6">
-                  Return to <Link to="/#0" className="text-blue-600 hover:underline transition duration-150 ease-in-out">Home </Link>
+                  Return to{" "}
+                  <Link
+                    to="/#0"
+                    className="text-blue-600 hover:underline transition duration-150 ease-in-out"
+                  >
+                    Home{" "}
+                  </Link>
                 </div>
               </div>
-
             </div>
           </div>
         </section>
-
       </main>
 
       <Banner />
-
     </div>
   );
 }
